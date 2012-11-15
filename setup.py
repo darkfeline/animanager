9eae4a868725c2658f8f71c3ecb1192ffb4d18ff
Allen Li <darkfeline@abagofapples.com>
2012-11-15 01:36:57 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import os
+import os.path
+import re
+
+
+def get_resources(dir):
+    l = []
+    _rget_resources(dir, l)
+    return l
+
+
+def _make_listing(dir):
+    return (dir, [os.path.join(dir, x) for x in os.listdir(dir) if
+            os.path.isfile(os.path.join(dir, x))])
+
+
+def _rget_resources(dir, l):
+    l.append(_make_listing(dir))
+    for d in os.listdir(dir):
+        if os.path.isdir(os.path.join(dir, d)):
+            _rget_resources(os.path.join(dir, d), l)
+
+
+def get_modules(dir):
+    return [_p_py.match(x).group(1) for x in os.listdir(dir) if _p_py.match(x)]
+_p_py = re.compile(r'(.+)\.py')
+
+
+def get_packages(dir):
+    l = []
+    _rget_packages(dir, '', l)
+    return l
+
+
+def _rget_packages(start, dir, l):
+    ls = os.listdir(os.path.join(start, dir))
+    if '__init__.py' in ls:
+        l.append(dir.replace('/', '.'))
+    for x in [x for x in ls if os.path.isdir(os.path.join(start, dir, x))]:
+        _rget_packages(start, os.path.join(dir, x), l)
+
+
+def get_scripts(dir):
+    l = []
+    _rget_scripts(dir, l)
+    return l
+
+
+def _rget_scripts(dir, l):
+    for f in os.listdir(dir):
+        f = os.path.join(dir, f)
+        if os.path.isfile(f):
+            l.append(f)
+        elif os.path.sidir(f):
+            _rget_scripts(f, l)
+
+setup(
+    name='Animanager',
+    version='0.2',
+    description='MySQL anime database cli application',
+    author='Allen Li',
+    author_email='darkfeline@abagofapples.com',
+    package_dir={'': 'src'},
+    py_modules=get_modules('src'),
+    packages=get_packages('src'),
+    scripts=get_scripts('src/bin')
+)
