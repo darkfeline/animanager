c391fd931bcf06e076e4911b08dce37ee2f5980f
Allen Li <darkfeline@abagofapples.com>
2013-01-10 17:54:12 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Animanager',
-    version='0.4',
+    version='0.5',
     description='Anime Manager MySQL frontend application',
     author='Allen Li',
     author_email='darkfeline@abagofapples.com',
