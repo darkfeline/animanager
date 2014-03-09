c6b189a41a92907a5123e14baac073cbed447858
Allen Li <darkfeline@abagofapples.com>
2014-03-09 18:41:33 -0400

    Fix manga add

--- src/animanager/manga/add.py
+++ src/animanager/manga/add.py
@@ -1,17 +1,17 @@
 import logging
 from datetime import date
 from urllib.parse import urlencode
-from xml.etree import ElementTree
 import sys
 
 from animanager import inputlib
 from animanager import mysqllib
+from animanager import xmllib
 from animanager.requestlib import ffrequest
 
 logger = logging.getLogger(__name__)
 
-mal_search = "http://myanimelist.net/api/anime/search.xml?"
-stati = ('plan to watch', 'watching', 'complete')
+mal_search = "http://myanimelist.net/api/manga/search.xml?"
+stati = ('plan to watch', 'reading', 'complete')
 
 
 def _get(e, key):
@@ -25,54 +25,50 @@ def main(config, name=None):
         name = input("Search for: ")
     response = ffrequest(mal_search + urlencode({'q': name}))
     response = response.read().decode()
-    try:
-        tree = ElementTree.fromstring(response)
-    except ElementTree.ParseError as e:
-        logger.error("Caught ParseError: {}".format(e))
-        logger.error(response)
-        print("Caught ParseError: {}".format(e))
-        line, col = e.position
-        response_lines = response.split('\n')
-        print(response_lines[line])
-        print(' ' * (col-1) + '^')
+    tree = xmllib.parse(response)
+    if tree is None:
         sys.exit(1)
     found = [
-        [_get(e, k) for k in ('id', 'title', 'episodes', 'type')]
+        [_get(e, k) for k in ('id', 'title', 'chapters', 'volumes', 'type')]
         for e in list(tree)]
     i = inputlib.get_choice(found)
 
-    # add anime entry
-    animedb_id, name, ep_total, type = found[i]
+    # add manga entry
+    mangadb_id, name, ch_total, vol_total, type = found[i]
     with mysqllib.connect(**config["db_args"]) as cur:
-        response = cur.execute('SELECT id FROM anime WHERE name=%s', (name,))
+        response = cur.execute(
+            'SELECT id FROM manga WHERE mangadb_id=%s', (mangadb_id,))
         if cur.fetchone():
             print("Already added")
             sys.exit(1)
         print("Adding main entry")
         cur.execute(' '.join((
-            'INSERT INTO anime (name, type, ep_total, animedb_id)',
+            'INSERT INTO manga (name, type, ch_total, vol_total, mangadb_id)',
             'VALUES',
-            '(%s, %s, %s, %s)',
-            )), (name, type, ep_total, animedb_id))
-        response = cur.execute('SELECT id FROM anime WHERE name=%s', (name,))
+            '(%s, %s, %s, %s, %s)',
+            )), (name, type, ch_total, vol_total, mangadb_id))
+        response = cur.execute(
+            'SELECT id FROM manga WHERE mangadb_id=%s', (mangadb_id,))
         id = cur.fetchone()[0]
 
-    # add myanime entry
+    # add mymanga entry
     print("Status:")
     for i, x in enumerate(stati):
         print('{}: {}'.format(i, x))
     i = int(input('Pick: '))
     status = stati[i]
     args = [id, status]
-    query = 'INSERT INTO myanime SET id=%s, status=%s'
-    if status == 'watching':
+    query = 'INSERT INTO mymanga SET id=%s, status=%s'
+    if status == 'reading':
         x = input('Set start date to today? [Y/n]')
         if x.lower() not in ('n', 'no'):
             query = ', '.join((query, 'date_started=%s'))
             args.append(date.today().isoformat())
     elif status == 'complete':
-        query = ', '.join((query, 'ep_watched=%s'))
-        args.append(ep_total)
+        query = ', '.join((query, 'ch_watched=%s'))
+        args.append(ch_total)
+        query = ', '.join((query, 'vol_watched=%s'))
+        args.append(vol_total)
         x = input('Set dates to today? [Y/n]')
         if x.lower() not in ('n', 'no'):
             query = ', '.join((query, 'date_started=%s', 'date_finished=%s'))
