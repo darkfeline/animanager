eeceecef095322da567992637b287f26017a920b
Allen Li <darkfeline@abagofapples.com>
2014-06-18 23:05:19 -0700

    Refactor manga/hold for merged dbs
    Also fix outright

--- src/animanager/manga/hold.py
+++ src/animanager/manga/hold.py
@@ -9,12 +9,11 @@ logger = logging.getLogger(__name__)
 
 def main(config, name=None):
     if not name:
-        name = input("Find an anime: ")
+        name = input("Find a manga: ")
     with mysqllib.connect(**config["db_args"]) as cur:
         cur.execute(' '.join((
-            'SELECT myanime.id, name FROM anime',
-            'LEFT JOIN myanime ON anime.id=myanime.id',
-            'WHERE status="watching"',
+            'SELECT id, name FROM manga',
+            'WHERE status="reading"',
             'AND name LIKE %s',
         )), ('%'+name+'%',))
         results = cur.fetchall()
@@ -23,5 +22,5 @@ def main(config, name=None):
             sys.exit(1)
         i = inputlib.get_choice(results)
         id = results[i][0]
-        cur.execute('UPDATE myanime SET status="on hold" WHERE id=%s', (id,))
+        cur.execute('UPDATE manga SET status="on hold" WHERE id=%s', (id,))
         print('Set to on hold')
