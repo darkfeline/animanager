7873cc58bbc84c5d987c8eba54f399cd549b9902
Allen Li <darkfeline@felesatra.moe>
2015-07-19 16:59:28 -0700

    Fix docstring

--- src/animanager/xmllib.py
+++ src/animanager/xmllib.py
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
-"""The module contains XML parsing code."""
+"""The module contains XML parsing code.
+
+Specifically, an XML preprocessing function for replacing HTML entities."""
 
 import re
 
