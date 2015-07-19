35676f81001b76f6fd6ccf882af0ae86d79a193f
Allen Li <darkfeline@felesatra.moe>
2015-07-19 16:52:18 -0700

    Rely on load_config default argument.

--- src/animanager/main/argparse.py
+++ src/animanager/main/argparse.py
@@ -30,7 +30,7 @@ def make_parser():
     # Set up main parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=configlib.load_config,
-                        default=configlib.default_config())
+                        default=None)
 
     # Set up subparsers
     subparsers = parser.add_subparsers(title='Managers')
