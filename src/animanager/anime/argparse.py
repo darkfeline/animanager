5bca35846a8d86bc226b5f169617bd1a6660cc42
Allen Li <darkfeline@felesatra.moe>
2015-07-19 16:51:39 -0700

    Require arguments for add and bump

--- src/animanager/anime/argparse.py
+++ src/animanager/anime/argparse.py
@@ -29,11 +29,11 @@ def add_parsers(subparsers):
     """Add subparsers."""
 
     parser = subparsers.add_parser('bump')
-    parser.add_argument('name', nargs='?')
+    parser.add_argument('name')
     parser.set_defaults(func=bump)
 
     parser = subparsers.add_parser('add')
-    parser.add_argument('name', nargs='?')
+    parser.add_argument('name')
     parser.set_defaults(func=add)
 
     parser = subparsers.add_parser('update')
