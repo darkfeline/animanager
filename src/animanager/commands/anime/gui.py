466038e3ae9617dd320475c770e8d0e7d5402131
Allen Li <darkfeline@felesatra.moe>
2015-12-23 06:44:26 -0800

    Working gui

--- src/animanager/commands/anime/gui.py
+++ src/animanager/commands/anime/gui.py
@@ -20,6 +20,8 @@ Automated anime watching and tracking script.
 """
 
 import logging
+import subprocess
+import os
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -39,6 +41,56 @@ def setup_parser(subparsers):
     parser.set_defaults(func=main)
 
 
+class YesNoDialog(Gtk.Dialog):
+
+    def __init__(self, parent, message):
+        Gtk.Dialog.__init__(self, 'Select', parent, 0,
+                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OK, Gtk.ResponseType.OK),
+                            flags=Gtk.DialogFlags.MODAL)
+        self.set_default_size(150, 100)
+        parentbox = self.get_content_area()
+
+        label = Gtk.Label(message)
+        parentbox.add(label)
+
+        self.show_all()
+
+
+class SelectDialog(Gtk.Dialog):
+
+    def __init__(self, parent, choices):
+        Gtk.Dialog.__init__(self, 'Select', parent, 0,
+                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OK, Gtk.ResponseType.OK),
+                            flags=Gtk.DialogFlags.MODAL)
+        self.set_default_size(150, 100)
+        parentbox = self.get_content_area()
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        parentbox.add(box)
+
+        store = Gtk.ListStore(int, str)
+        view = Gtk.TreeView(store)
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn('Choice', renderer, text=1)
+        view.append_column(column)
+        box.pack_start(view, True, True, 2)
+
+        select = view.get_selection()
+        select.connect("changed", self._on_selection_changed)
+
+        for i, x in enumerate(choices):
+            store.append([i, x])
+
+        self.show_all()
+
+    def _on_selection_changed(self, selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is not None:
+            self.selection = model[treeiter][0]
+
+
 class MainWindow(Gtk.Window):
 
     def __init__(self, config, db):
@@ -50,20 +102,17 @@ class MainWindow(Gtk.Window):
         mainbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.add(mainbox)
 
-        self.store = Gtk.ListStore(int, str, int, int, bool)
-        self.view = Gtk.TreeView(self.store)
+        self.store = Gtk.ListStore(int, int, str, int, int, bool)
+        view = Gtk.TreeView(self.store)
         renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("id", renderer, text=0)
-        self.view.append_column(column)
-        column = Gtk.TreeViewColumn("series", renderer, text=1)
-        self.view.append_column(column)
-        column = Gtk.TreeViewColumn("cur.", renderer, text=2)
-        self.view.append_column(column)
-        column = Gtk.TreeViewColumn("avail.", renderer, text=3)
-        self.view.append_column(column)
-        column = Gtk.TreeViewColumn("missing?", renderer, text=4)
-        self.view.append_column(column)
-        mainbox.pack_start(self.view, True, True, 2)
+        for i, name in enumerate(['id', 'series', 'cur.', 'avail.',
+                                  'missing?']):
+            column = Gtk.TreeViewColumn(name, renderer, text=i+1)
+            view.append_column(column)
+            mainbox.pack_start(view, True, True, 2)
+
+        select = view.get_selection()
+        select.connect("changed", self._on_selection_changed)
 
         subbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         mainbox.pack_start(subbox, True, True, 2)
@@ -76,7 +125,16 @@ class MainWindow(Gtk.Window):
         button.connect("clicked", self.watch_show)
         subbox.pack_start(button, True, True, 2)
 
+        button = Gtk.Button(label="Bump")
+        button.connect("clicked", self.bump_show)
+        subbox.pack_start(button, True, True, 2)
+
         self.load_files_to_watch()
+
+    def _on_selection_changed(self, selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is not None:
+            self.selection = model[treeiter]
 
     def load_files_to_watch(self, widget=None):
         # Load series information.
@@ -91,21 +149,64 @@ class MainWindow(Gtk.Window):
         # Clean up unneeded files.
         for x in series_list:
             x.clean()
-        # Remove series that no longer have any files.
+            # Remove series that no longer have any files.
         series_list = [x for x in series_list if x]
 
         self.series_list = series_list
+
+    @property
+    def series_list(self):
+        return self._series_list
+
+    @series_list.setter
+    def series_list(self, value):
+        self._series_list = value
         self._update_view()
 
     def _update_view(self):
         """Update GUI view using series_list."""
         self.store.clear()
-        for info in self.series_list:
-            self.store.append([info.id, info.name, info.ep_watched, len(info),
-                               not info.has_next()])
+        for i, info in enumerate(self.series_list):
+            self.store.append([i, info.id, info.name, info.ep_watched,
+                               len(info), not info.has_next()])
 
     def watch_show(self, widget=None):
-        pass
+        info = self.series_list[self.selection[0]]
+        # Check if we have the next episode.
+        if not info.has_next():
+            return
+
+        # Choose the file to play if there is more than one.
+        files = info.peek()
+        if len(files) > 1:
+            dialog = SelectDialog(self, [os.path.basename(x) for x in files])
+            response = dialog.run()
+            dialog.destroy()
+            if response == Gtk.ResponseType.OK:
+                i = dialog.selection
+            else:
+                return
+            file = files[i]
+        else:
+            file = files[0]
+        del files
+
+        # Play the episode.
+        subprocess.Popen(self.config['watch'].getargs('player') + [file])
+
+    def bump_show(self, widget=None):
+        info = self.series_list[self.selection[0]]
+
+        # Check if we have the next episode.
+        if not info.has_next():
+            return
+
+        self.db.bump(info.id)
+        # Remove the episode from our list.
+        info.pop()
+
+        # Filter out series that have no files.
+        self.series_list = [x for x in self.series_list if x]
 
 
 def main(args):
