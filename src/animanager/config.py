eacb2303920f432f477c8f3983411b22514d08e2
Allen Li <darkfeline@felesatra.moe>
2016-02-22 00:28:31 -0800

    New config

--- src/animanager/config.py
+++ src/animanager/config.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015  Allen Li
+# Copyright (C) 2015-2016  Allen Li
 #
 # This file is part of Animanager.
 #
@@ -15,40 +15,21 @@
 # You should have received a copy of the GNU General Public License
 # along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
-"""This module contains stuff related to configuration."""
-
 import configparser
 import os
 import re
 import shlex
-from collections import defaultdict
 
 
 class Config:
 
-    """Config class.
-
-    This class handles configuration loading, saving, checking, and
-    manipulation.
-
-    """
-
     DEF_PATH = os.path.join(os.environ['HOME'], '.animanager', 'config.ini')
     DEF_VALUES = {
-        'general': {
+        'anime': {
             'database': '~/.animanager/database.db',
-        },
-        'watch': {
+            'watchdir': '~/anime',
             'player': 'mpv',
         },
-        'series': {},
-        'mal_args': {},
-    }
-    REQ_VALUES = {
-        'general': ['database'],
-        'watch': ['player', 'directory'],
-        'series': [],
-        'mal_args': ['user', 'passwd'],
     }
     _CONVERTERS = {
         'path': os.path.expanduser,
@@ -63,21 +44,6 @@ class Config:
             converters=self._CONVERTERS)
         self.config.read_dict(self.DEF_VALUES)
         self.config.read(self.path)
-        self._check()
-
-    def _check(self):
-        """Check that all required configuration keys have been provided.
-
-        Raises MissingConfigKeysError.
-
-        """
-        missing = defaultdict(list)
-        for section in self.REQ_VALUES:
-            for key in self.REQ_VALUES[section]:
-                if key not in self.config[section]:
-                    missing[section].append(key)
-        if missing:
-            raise MissingConfigKeysError(missing)
 
     def save(self):
         with open(self.path, 'w') as file:
@@ -86,6 +52,7 @@ class Config:
     def __getitem__(self, key):
         return self.config[key]
 
+    # XXX Move
     def register(self, db, id):
         results = db.select(
             table='anime',
@@ -107,19 +74,3 @@ class Config:
 
     def unregister(self, id):
         del self['series'][str(id)]
-
-
-class ConfigError(Exception):
-    pass
-
-
-class MissingConfigKeysError(ConfigError):
-
-    def __init__(self, missing):
-        self.missing = missing
-
-    def __str__(self):
-        return "Missing configuration values: {}".format(
-            '; '.join(
-                'in [{}], {}'.format(section, ', '.join(self.missing[section]))
-                for section in self.missing))
