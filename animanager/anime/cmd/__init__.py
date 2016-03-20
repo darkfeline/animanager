2985f6441a817da80437d33ea60cb78f1aabe0d1
Allen Li <darkfeline@felesatra.moe>
2016-03-20 14:26:49 -0700

    Split GPL commands

--- animanager/anime/cmd/__init__.py
+++ animanager/anime/cmd/__init__.py
@@ -29,12 +29,13 @@ import traceback
 from tabulate import tabulate
 
 from animanager import __version__ as VERSION
+from animanager.anime import anidb
+from animanager.anime import watchlib
+from animanager.anime.db import AnimeDB
+from animanager.anime.results import AIDResults
+from animanager.anime.results import AIDResultsManager
 
-from . import anidb
-from . import watchlib
-from .db import AnimeDB
-from .results import AIDResults
-from .results import AIDResultsManager
+from . import gpl
 
 logger = logging.getLogger(__name__)
 
@@ -394,74 +395,4 @@ class AnimeCmd(Cmd):
         # Print
         pass  # XXX
 
-    ###########################################################################
-    # GPL information.
-    GPL_COPYING = dedent('''\
-    Animanager is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
-
-    Animanager is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public
-    License along with this program. If not, see
-    <http://www.gnu.org/licenses/gpl.html>.''')
-
-    GPL_WARRANTY = dedent('''\
-    Animanager is distributed WITHOUT ANY WARRANTY. The following
-    sections from the GNU General Public License, version 3, should
-    make that clear.
-
-    15. Disclaimer of Warranty.
-
-    THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
-    APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
-    HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT
-    WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-    PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE
-    OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
-    ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
-
-    16. Limitation of Liability.
-
-    IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-    WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR
-    CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES,
-    INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES
-    ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT
-    NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES
-    SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO
-    OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY
-    HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-
-    17. Interpretation of Sections 15 and 16.
-
-    If the disclaimer of warranty and limitation of liability provided
-    above cannot be given local legal effect according to their terms,
-    reviewing courts shall apply local law that most closely approximates
-    an absolute waiver of all civil liability in connection with the
-    Program, unless a warranty or assumption of liability accompanies a
-    copy of the Program in return for a fee.
-
-    See <http://www.gnu.org/licenses/gpl.html>, for more details.''')
-
-    def help_gpl(self):
-        print(dedent('''\
-        Show GPL information.
-
-        "gpl c" to show copying information.
-        "gpl w" to show warranty information.'''))
-
-    def do_gpl(self, arg):
-        """Show GPL information."""
-        if arg == 'c':
-            print(self.GPL_COPYING)
-        elif arg == 'w':
-            print(self.GPL_WARRANTY)
-        else:
-            self.do_help('gpl')
+gpl.register_commands(AnimeCmd)
