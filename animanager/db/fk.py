ef14bbcaf87899507c87f179df13625e87eec2df
Allen Li <darkfeline@felesatra.moe>
2016-03-20 16:12:14 -0700

    Remove unused import

--- animanager/db/fk.py
+++ animanager/db/fk.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
-from abc import ABC, abstractmethod
-
 from . import migrations
 from .base import DatabaseError
 from .sqlite import SQLiteDB
