9902fa84b61ab5a0c10b020da1ad6c3534b65318
Allen Li <darkfeline@felesatra.moe>
2016-04-21 23:26:22 -0700

    Start porting to query package

---
+++ animanager/animedb/query/eptype.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2015-2016  Allen Li
+#
+# This file is part of Animanager.
+#
+# Animanager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Animanager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Episode type queries."""
+
+from collections import namedtuple
+from typing import Iterator, Iterable
+from weakref import WeakKeyDictionary
+
+from .collections import Episode
+
+EpisodeType = namedtuple(
+    'EpisodeType',
+    ['id', 'name', 'prefix'])
+
+
+def get_episode_types(db) -> Iterator[EpisodeType]:
+    """Get all episode types."""
+    cur = db.cursor()
+    cur.execute('SELECT id, name, prefix FROM episode_type')
+    for type_id, name, prefix in cur:
+        yield EpisodeType(type_id, name, prefix)
+
+
+class EpisodeTypes:
+
+    """A class for storing all episode types.
+
+    >>> x = EpisodeTypes([
+    ...     EpisodeType(1, 'foo', 'F'),
+    ...     EpisodeType(2, 'bar', 'B')])
+    >>> x['foo']
+    EpisodeType(id=1, name='foo', prefix='F')
+    >>> x[2]
+    EpisodeType(id=2, name='bar', prefix='B')
+    >>> x[3]
+    Traceback (most recent call last):
+        ...
+    KeyError: 3
+
+    """
+
+    def __init__(self, episode_types: Iterable[EpisodeType]) -> None:
+        self.types = tuple(episode_types)
+        self.by_id = {}
+        self.by_name = {}
+        for episode_type in self.types:
+            self.by_id[episode_type.id] = episode_type
+            self.by_name[episode_type.name] = episode_type
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.by_id[key]
+        elif isinstance(key, str):
+            return self.by_name[key]
+        else:
+            raise KeyError(key)
+
+    _db_cache = WeakKeyDictionary()
+
+    @classmethod
+    def from_db(cls, db, force=False) -> 'EpisodeTypes':
+        """Make instance from database.
+
+        For performance, this caches the episode types for the database.  The
+        `force` parameter can be used to bypass this.
+
+        """
+        if force or db not in cls._db_cache:
+            cls._db_cache[db] = cls(get_episode_types(db))
+        return cls._db_cache[db]
+
+    @classmethod
+    def forget(cls, db):
+        """Forget cache for database."""
+        cls._db_cache.pop(db, None)
+
+    def get_epno(self, episode: Episode):
+        """Return epno for an Episode instance.
+
+        epno is a string formatted with the episode number and type, e.g., S1,
+        T2.
+
+        >>> x = EpisodeTypes([EpisodeType(1, 'foo', 'F')])
+        >>> ep = Episode(type=1, number=2)
+        >>> x.get_epno(ep)
+        'F2'
+
+        """
+        return '{}{}'.format(self[episode.type].prefix, episode.number)
---
+++ animanager/animedb/query/status.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2015-2016  Allen Li
+#
+# This file is part of Animanager.
+#
+# Animanager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Animanager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Status related queries."""
+
+from typing import Any, Iterator
+
+from .eptype import EpisodeTypes
+
+
+def cache_status(db, aid, force=False):
+    """Calculate and cache status for given anime.
+
+    Don't do anything if status already exists and force is False.
+
+    """
+    with db:
+        cur = db.cursor()
+        if not force:
+            # We don't do anything if we already have this aid in our
+            # cache.
+            cur.execute('SELECT 1 FROM cache_anime WHERE aid=?', (aid,))
+            if cur.fetchone() is not None:
+                return
+
+        # Retrieve information for determining complete.
+        cur.execute(
+            'SELECT episodecount, enddate FROM anime WHERE aid=?', (aid,))
+        row = cur.fetchone()
+        if row is None:
+            raise ValueError('aid provided does not exist')
+        episodecount, enddate = row
+
+        # Select all regular episodes in ascending order.
+        cur.execute("""
+            SELECT number, user_watched FROM episode
+            WHERE aid=? AND type=?
+            ORDER BY number ASC
+            """, (aid, EpisodeTypes.from_db(db)['regular'].id))
+
+        # We find the last consecutive episode that is user_watched.
+        number = 0
+        for number, watched in cur:
+            # Once we find the first unwatched episode, we set the last
+            # consecutive watched episode to the previous episode (or 0).
+            if watched == 0:
+                number -= 1
+                break
+        # We store this in the cache.
+        set_status(db, aid, enddate and episodecount <= number, number)
+
+def set_status(
+        db,
+        aid: int,
+        complete: Any,
+        watched_episodes: int,
+) -> None:
+    """Set anime status."""
+    with db:
+        cur = db.cursor()
+        cur.execute(
+            """UPDATE cache_anime
+            SET complete=?, watched_episodes=?
+            WHERE aid=?""",
+            (complete, watched_episodes, aid))
+        if db.changes() == 0:
+            cur.execute(
+                """INSERT INTO cache_anime
+                (aid, complete, watched_episodes)
+                VALUES (?, ?, ?)""",
+                (aid, 1 if complete else 0, watched_episodes))
+
+
+def get_complete(db) -> Iterator[int]:
+    """Return AID of complete anime."""
+    cur = db.cursor()
+    cur.execute(
+        """SELECT aid FROM cache_anime
+        WHERE complete=?""", (1,))
+    for row in cur:
+        yield row[0]
