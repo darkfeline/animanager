b9ccaecaa6883ec93bd1d24537f37b13e4e47ce0
Allen Li <darkfeline@felesatra.moe>
2016-04-22 22:17:20 -0700

    Refactor select

--- animanager/animedb/query/select.py
+++ animanager/animedb/query/select.py
@@ -18,7 +18,7 @@
 """Select type queries."""
 
 import logging
-from typing import Any, Iterable, Iterator, Mapping, Optional, Sequence, Union
+from typing import Any, Iterable, Iterator, Mapping, Sequence, Union
 
 from .collections import Anime, Episode
 from .status import cache_status
@@ -27,28 +27,7 @@ logger = logging.getLogger(__name__)
 
 SQLParams = Union[Sequence[Any], Mapping[str, Any]]
 
-
-def select(db, where_query: str, params: SQLParams,) -> Iterator[int]:
-    """Perform an arbitrary SQL SELECT WHERE on the anime table.
-
-    By nature of "arbitrary query", this is vulnerable to injection, use only
-    trusted values for `where_query`.
-
-    :returns: an iterator of AIDs
-
-    """
-    aids = db.cursor()
-    aids.execute(
-        """SELECT aid FROM anime
-        LEFT JOIN watching USING (aid)
-        WHERE {}
-        ORDER BY aid ASC
-        """.format(where_query),
-        params)
-    for aid in aids:
-        yield aid
-
-LOOKUP_ANIME_FIELDS = {
+ANIME_FIELDS = {
     'aid': 'anime.aid',
     'title': 'title',
     'type': 'type',
@@ -59,7 +38,7 @@ LOOKUP_ANIME_FIELDS = {
     'regexp': 'regexp',
 }
 
-LOOKUP_EPISODE_FIELDS = {
+EPISODE_FIELDS = {
     'aid': 'aid',
     'type': 'type',
     'number': 'number',
@@ -68,23 +47,90 @@ LOOKUP_EPISODE_FIELDS = {
     'user_watched': 'user_watched',
 }
 
+ALL = 1
+"""Flag for selecting all fields."""
 
-def _clean_fields(allowed_fields: dict, fields: Optional[Iterable]):
+FieldsParam = Union[Iterable[str], int]
+
+
+def _clean_fields(allowed_fields: dict, fields: FieldsParam) -> Iterable[str]:
     """Clean lookup fields and check for errors."""
-    if fields is None:
+    if fields == ALL:
         fields = allowed_fields.keys()
-    fields = tuple(fields)
-    unknown_fields = set(fields) & allowed_fields.keys()
-    if unknown_fields:
-        raise ValueError('Unknown fields: {}'.format(unknown_fields))
+    else:
+        fields = tuple(fields)
+        unknown_fields = set(fields) & allowed_fields.keys()
+        if unknown_fields:
+            raise ValueError('Unknown fields: {}'.format(unknown_fields))
     return fields
+
+
+def select(
+        db,
+        where_query: str,
+        where_params: SQLParams,
+        fields: FieldsParam = ALL,
+        episode_fields: FieldsParam = (),
+) -> Iterator[Anime]:
+    """Perform an arbitrary SQL SELECT WHERE on the anime table.
+
+    By nature of "arbitrary query", this is vulnerable to injection, use only
+    trusted values for `where_query`.
+
+    :param str where_query: SELECT WHERE query
+    :param where_params: parameters for WHERE query
+    :param fields: anime fields to get.  If :const:`ALL`, get all fields.
+        Default is :const:`ALL`.
+    :param episode_fields: episode fields to get.
+        If :const:`ALL`, get all fields.  If empty, don't get episodes.
+        `fields` must contain 'aid' to get episodes.
+    :returns: iterator of Anime
+
+    """
+
+    fields = _clean_fields(ANIME_FIELDS, fields)
+    if not fields:
+        raise ValueError('Fields cannot be empty')
+    if 'aid' in fields:
+        episode_fields = _clean_fields(ANIME_FIELDS, episode_fields)
+    else:
+        episode_fields = ()
+
+    with db:
+        anime_query = """
+            SELECT {}
+            FROM anime
+                JOIN cache_anime USING (aid)
+                LEFT JOIN watching USING (aid)
+            WHERE {}""".format(where_query)
+        anime_query = anime_query.format(
+            ','.join(ANIME_FIELDS[field] for field in fields))
+
+        anime_rows = db.cursor().execute(anime_query, where_params)
+        for row in anime_rows:
+            anime = Anime(**{
+                field: value
+                for field, value in zip(fields, row)})
+
+            if episode_fields:
+                episode_query = 'SELECT {} FROM episode WHERE aid=?'
+                episode_query = episode_query.format(
+                    ','.join(EPISODE_FIELDS[field] for field in episode_fields))
+
+                episode_rows = db.cursor().execute(episode_query, (anime.aid,))
+                episodes = [
+                    Episode(**{
+                        field: value
+                        for field, value in zip(episode_fields, row)})
+                    for row in episode_rows]
+                anime.episodes = episodes
+
+            yield anime
 
 
 def lookup(
         db,
         aid: int,
-        fields: Optional[Iterable] = None,
-        episode_fields: Optional[Iterable] = (),
 ) -> Anime:
     """Look up full information for a single anime.
 
@@ -95,45 +141,11 @@ def lookup(
         If ``None``, get all fields.  If empty, don't get episodes.
 
     """
-
-    fields = _clean_fields(LOOKUP_ANIME_FIELDS, fields)
-    if not fields:
-        raise ValueError('Fields cannot be empty')
-    episode_fields = _clean_fields(LOOKUP_ANIME_FIELDS, episode_fields)
-
-    cache_status(db, aid, force=True)
-    with db:
-        cur = db.cursor()
-        anime_query = """
-            SELECT {}
-            FROM anime
-                JOIN cache_anime USING (aid)
-                LEFT JOIN watching USING (aid)
-            WHERE anime.aid=?"""
-        anime_query = anime_query.format(
-            ','.join(LOOKUP_ANIME_FIELDS[field] for field in fields))
-
-        cur.execute(anime_query, (aid,))
-        row = cur.fetchone()
-        if row is None:
-            raise ValueError('Invalid aid')
-        anime = Anime(**{
-            field: value
-            for field, value in zip(fields, row)})
-
-        if episode_fields:
-            episode_query = 'SELECT {} FROM episode WHERE aid=?'
-            episode_query = episode_query.format(
-                ','.join(
-                    LOOKUP_EPISODE_FIELDS[field]
-                    for field in episode_fields))
-
-            cur.execute(episode_query, (aid,))
-            episodes = [
-                Episode(**{
-                    field: value
-                    for field, value in zip(episode_fields, row)})
-                for row in cur]
-            anime.episodes = episodes
-
-        return anime
+    cache_status(db, aid)
+    return next(select(
+        db,
+        'aid=?',
+        (aid,),
+        fields=ALL,
+        episode_fields=ALL,
+    ))
