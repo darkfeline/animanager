3423e1480b23c488109dbd7c1d8fad99ef14a922
Allen Li <darkfeline@felesatra.moe>
2016-09-11 01:56:11 -0700

    Repackage

--- setup.py
+++ setup.py
@@ -5,6 +5,20 @@ from setuptools import find_packages, setup
 setup(
     name='animanager',
     version='0.10.2',
+    description='Command line program for advanced anime watching management',
+    long_description='',
+    keywords='',
+    url='https://www.felesatra.moe/animanager/',
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3.5',
+    ],
+
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=['apsw', 'tabulate'],
     entry_points={
@@ -12,10 +26,4 @@ setup(
             'animanager = animanager.__main__:main',
         ],
     },
-
-    author='Allen Li',
-    author_email='darkfeline@felesatra.moe',
-    description='Command line program for advanced anime watching management',
-    license='GPLv3',
-    url='https://www.felesatra.moe/animanager/',
 )
