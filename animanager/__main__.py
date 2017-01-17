c06ddf558942df173084f23601ba72690ca6d3e0
Allen Li <darkfeline@felesatra.moe>
2017-01-17 00:14:06 -0800

    Clean up main

--- animanager/__main__.py
+++ animanager/__main__.py
@@ -15,11 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
+import abc
 import argparse
 import logging
 import logging.config
 
-from animanager import subcmds
+from animanager.animecmd import AnimeCmd
+from animanager.config import Config
 
 
 def main():
@@ -36,7 +38,12 @@ def main():
 
 
 def _make_parser():
-    # Set up main parser.
+    parser = _make_root_parser()
+    _add_command_parsers(parser)
+    return parser
+
+
+def _make_root_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config',
                         default='',
@@ -44,13 +51,13 @@ def _make_parser():
     parser.add_argument('--debug',
                         action='store_true',
                         help='Enable debug output.')
-
-    # Set up subparsers.
-    subparsers = parser.add_subparsers(title='Managers')
-    for subcommand in subcmds.subcommands:
-        subcommand.setup_parser(subparsers)
-
     return parser
+
+
+def _add_command_parsers(parser):
+    subparsers = parser.add_subparsers(title='Commands')
+    for command in _commands:
+        command.setup_parser(subparsers)
 
 
 def _setup_logging(args):
@@ -76,6 +83,45 @@ def _setup_logging(args):
         },
         'disable_existing_loggers': False,
     })
+
+
+_commands = []
+
+
+def _add_command(command):
+    """Decorator for adding command classes."""
+    _commands.append(command())
+    return command
+
+
+class _Command(metaclass=abc.ABCMeta):
+
+    """ABC for a main script command."""
+
+    @abc.abstractmethod
+    def setup_parser(self, subparsers):
+        """Add subparser for command."""
+
+    @abc.abstractmethod
+    def main(self, args):
+        """Subroutine for command."""
+
+
+@_add_command
+class _AnimeCommand(_Command):
+
+    def setup_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            'anime',
+            description='Anime manager.',
+            help='Start anime manager.',
+        )
+        parser.set_defaults(func=self.main)
+
+    def main(self, args):
+        config = Config(args.config)
+        cmd = AnimeCmd(config)
+        cmd.cmdloop()
 
 
 if __name__ == '__main__':
