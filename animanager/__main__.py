2ecdc1cbd4f7359893c90f4a5ea7c5e357a6390e
Allen Li <darkfeline@felesatra.moe>
2018-04-01 19:05:30 -0700

    Remove subcommands

--- animanager/__main__.py
+++ animanager/__main__.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
-import abc
 import argparse
 import logging
 import logging.config
@@ -30,17 +29,11 @@ def main():
     parser = _make_parser()
     args = parser.parse_args()
     _setup_logging(args)
-    try:
-        func = args.func
-    except AttributeError:
-        parser.print_help()
-    else:
-        func(args)
+    _main(args)
 
 
 def _make_parser():
     parser = _make_root_parser()
-    _add_command_parsers(parser)
     return parser
 
 
@@ -53,12 +46,6 @@ def _make_root_parser():
                         action='store_true',
                         help='Enable debug output.')
     return parser
-
-
-def _add_command_parsers(parser):
-    subparsers = parser.add_subparsers(title='Commands')
-    for command in _commands:
-        command.setup_parser(subparsers)
 
 
 def _setup_logging(args):
@@ -86,47 +73,10 @@ def _setup_logging(args):
     })
 
 
-_commands = []
-
-
-def _add_command(command):
-    """Decorator for adding command classes."""
-    _commands.append(command())
-    return command
-
-
-class _Command(metaclass=abc.ABCMeta):
-
-    """ABC for a main script command."""
-
-    @abc.abstractmethod
-    def setup_parser(self, subparsers):
-        """Add subparser for command."""
-
-    @abc.abstractmethod
-    def main(self, args):
-        """Subroutine for command."""
-
-
-@_add_command
-class _AnimeCommand(_Command):
-
-    def setup_parser(self, subparsers):
-        parser = subparsers.add_parser(
-            'anime',
-            description='Anime manager.',
-            help='Start anime manager.',
-        )
-        parser.set_defaults(func=self.main)
-
-    def main(self, args):
-        if args.acle:
-            config = Config(args.config)
-            acle.start_command_line(config)
-        else:
-            config = Config(args.config)
-            cmd = AnimeCmd(config)
-            cmd.cmdloop()
+def _main(args):
+    config = Config(args.config)
+    cmd = AnimeCmd(config)
+    cmd.cmdloop()
 
 
 if __name__ == '__main__':
